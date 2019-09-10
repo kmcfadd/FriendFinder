8297a4a71fd8ff9ff6ecb67eec96e033d2ef0407
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-09-10 13:29:03 -0700

    get server.js code ready to take in all data and begin listening

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var path = require('path');
+var friends = require("../data/friends");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+module.exports = function (app) {
 
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.json());
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
 
-app.get("/api/friends", )
+    })
 
-app.post("/api/friends", )
+    app.post("/api/friends", function (req, res) {
+        console.log(req.body);
+
+    })
+
+}
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+// our dependencies for this app to give server functionality
 var express = require('express');
 var path = require('path');
 
+// configuring express server
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// sets up express to handle data parsing
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
+
+// handles our server routing when requesting or sending data
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+// starts the server 
+app.listen(PORT, function () {
+    console.log("App listening on port " + PORT);
+});
