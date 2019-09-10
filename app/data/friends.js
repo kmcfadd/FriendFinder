79fe8e7b44f52e3d6ca56509850cf1de35d3e670
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-09-10 13:19:50 -0700

    add some module exporting for the data and routing

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendArray = [{
+    name: "Dude",
+    photo: "https://whateverimage.jpg",
+    scores: [
+        1,
+        2,
+        3,
+        4,
+        5,
+        1,
+        5,
+        3,
+        5,
+        4
+    ]
+}];
+
+module.exports = friendArray;
