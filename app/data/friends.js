d8697a4adf5f4b49f585bd35541d8892c557ed0d
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-09-25 12:24:17 -0700

    add matching logic to the post request after sending survey data:

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,35 @@
-var friendArray = [{
-    name: "Dude",
-    photo: "https://whateverimage.jpg",
-    scores: [
-        1,
-        2,
-        3,
-        4,
-        5,
-        1,
-        5,
-        3,
-        5,
-        4
-    ]
-}];
+var friends = [{
+        name: "Dude",
+        photo: "https://whateverimage.jpg",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            5,
+            3,
+            5,
+            4
+        ]
+    },
+    {
+        name: "Bro",
+        photo: "https://anotherimage.jpg",
+        scores: [
+            3,
+            5,
+            4,
+            1,
+            2,
+            5,
+            3,
+            4,
+            1,
+            2
+        ]
+    }
+];
 
-module.exports = friendArray;
+module.exports = friends
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,33 @@ module.exports = function (app) {
     })
 
     app.post("/api/friends", function (req, res) {
-        console.log(req.body);
+        var bestBud = {
+            name: "",
+            photo: "",
+            scoreDif: 5
+        }
 
+        var userData = req.body
+        var userScores = userData.scores
+
+        var totalDif = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+            totalDif = 0;
+
+            for (var j = 0; j < 10; j++) {
+                totalDif = Math.abs(parseInt(userScores[j] - parseInt(friends[i].scores[j])))
+            }
+
+            if (totalDif <= bestBud.scoreDif) {
+                bestBud.name = friends[i].name
+                bestBud.photo = friends[i].photo
+                bestBud.scoreDif = totalDif;
+            }
+        }
+        friends.push(userData)
+
+        res.json(bestBud)
     })
 
 }
